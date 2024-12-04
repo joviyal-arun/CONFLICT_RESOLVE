7638b02db7064801ad375f2bb11a45f02d4f96a4
joviyal-arun <joviyalarun07@gmail.com>
2024-12-04 16:20:26 +0530

    changes

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
 def greet():
     print("Hello,world")
-
-
-def arunachalam_off():
-    return "welcome to arunachalam off"
-def arunachalam_personal_laptop():
-    return "welcome to arunachalam personal laptop"
